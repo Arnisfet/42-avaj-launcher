b9d282ab9ab72f58757b085700a6b068ba1de1a7
Arnisfet <mrudge@student.21-school.ru>
2023-09-27 23:18:34 +0300

    Intial structure commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        long a = 128;
+        System.out.printf("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println(a);
+    }
+}
