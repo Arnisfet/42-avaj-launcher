e0fea6ceab1199be6192507f2735be19c4c95d00
Arnisfet <mrudge@student.21-school.ru>
2023-10-10 23:02:13 +0300

    Add the exception class. Need to test

--- src/com/github/arnisfet/avajlauncher/Simulator.java
+++ src/com/github/arnisfet/avajlauncher/Simulator.java
@@ -1,8 +1,14 @@
 package src.com.github.arnisfet.avajlauncher;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+
 public class Simulator {
 	
 	private static BufferedReader buf_reader;
+	private static int simulationCycles;
 
 	public static void main(String[] args) {
 		if (args.length != 1) {
@@ -10,9 +16,20 @@ public class Simulator {
 			return ;
 		}
 	}
-	public static InitSimulation (File file) {
-		buf_reader = new BufferedReader(new FileReader(file));
+	
+	public static void InitSimulation (File file) throws SimulationException {
+		try {
+			buf_reader = new BufferedReader(new FileReader(file));
+			simulationCycles = Integer.parseInt(buf_reader.readLine());
+			System.out.println(simulationCycles);
+		} catch (NumberFormatException | IOException e) {
+			throw new SimulationException(e);
+		}
 	}
 	
+	public static class SimulationException extends Exception {
+		public SimulationException() { super(); }
+		public SimulationException(String exception) { super(exception); }
+		public SimulationException(Throwable exception) { super(exception); }
 	}
 }
